766812c1a45a52f3882c7917d5f984942f522e21
Aran <h2obuffalo@users.noreply.github.com>
2019-02-07 16:57:41 +0000

    added interactive elements and nav to stegosaurus profile

---
+++ js/game.js
@@ -0,0 +1,21 @@
+ ((d) => {
+ 
+
+  function allowDrop(ev)
+  {
+    ev.preventDefault();
+  }
+  
+  function drag(ev)
+  {
+    ev.dataTransfer.setData("Text",ev.target.id);
+  }
+  
+  function drop(ev)
+  {
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+  
+  })(document);
