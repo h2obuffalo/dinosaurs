0d03e934f98e7f717f8e01b91406641993dbcfec
Aran <h2obuffalo@users.noreply.github.com>
2019-02-08 16:26:06 +0000

    updates

--- js/game2.js
+++ js/game2.js
@@ -4,6 +4,7 @@
   let space1 = d.getElementById("space1");
   let space2 = d.getElementById("space2");
   let dragged = "";
+  let wordsRight = 0;
 
   /* events fired on the draggable target */
  d.addEventListener("drag", function( event ) {
@@ -52,26 +53,31 @@
         //if the word entered doesn't match, change background colour
         if (event.target.id === "space1" && dragged.id === "word5") {      
           event.target.replaceWith(dragged);
+          wordsRight += 1;
       }
                event.target.style.background = "red";
 
         if (event.target.id === "space2" && dragged.id === "word1") {      
           event.target.replaceWith(dragged);
+          wordsRight += 1;
       }
                event.target.style.background = "red";
 
         if (event.target.id === "space3" && dragged.id === "word2") {      
           event.target.replaceWith(dragged);
+          wordsRight += 1;
       }
                event.target.style.background = "red";
 
         if (event.target.id === "space4" && dragged.id === "word3") {      
           event.target.replaceWith(dragged);
+          wordsRight += 1;
       }
                event.target.style.background = "red";
 
         if (event.target.id === "space5" && dragged.id === "word4") {      
           event.target.replaceWith(dragged);
+          wordsRight += 1;
       }
                event.target.style.background = "red";
 
